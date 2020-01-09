c29153c88dbc26df06f07873697a020df3d5752d
夏飞 <xiafei571@gmail.com>
2020-01-09 14:16:09 +0900

    添加部门

--- src/edu201909/spring/controller/DeptController.java
+++ src/edu201909/spring/controller/DeptController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,11 +25,24 @@ public class DeptController {
 	public List<DeptInfo> getDeptList() {
 		return deptService.getDeptList();
 	}
-	
+
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String getDeptIndex(ModelMap modelMap) {
 		List<DeptInfo> list = deptService.getDeptList();
 		modelMap.put("list", list);
 		return "dept";
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String addDept(ModelMap modelMap) {
+		modelMap.put("deptInfo", new DeptInfo());
+		return "add_dept";
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String addDept(ModelMap modelMap, @ModelAttribute DeptInfo deptInfo) {
+		// TODO 把数据插入数据库
+		System.out.println(deptInfo);
+		return "redirect:/dept/index";
 	}
 }
